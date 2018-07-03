38cd787f1c4cdc8ccfda9ba097ff9ea88b1f373f
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-03 17:05:03 -0400

    Added my working solution on the Sum All Odd Fibonacci Numbers Challenge

---
+++ fcc.js
@@ -0,0 +1,19 @@
+// Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers
+
+function sumFibs(num) {
+  let next = 1;
+  let prev = 0;
+  let fiboArr = [];
+  for(let i = 0; i < num; i++){
+    let curr = prev + next;
+    prev = next;
+    next = curr;
+    if(curr % 2 !== 0 && curr <= num){
+      fiboArr.push(curr);
+    }
+  }
+ let final = fiboArr.reduce((a, b)=> (a+b));
+  return final + 1;
+}
+
+sumFibs(1000);
