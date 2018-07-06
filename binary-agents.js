99d5b993425ec17c3b86007c398c5177014d837b
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-06 13:22:27 -0400

    Adding the solution for the binary agents challenge

---
+++ binary-agents.js
@@ -0,0 +1,11 @@
+// Algo Scripting: Binary Agents
+
+function binaryAgent(str) {
+  let binarySplit = str.split(" ");
+  let mappedBinary = binarySplit.map(function(item){
+    return String.fromCharCode(parseInt(item, 2));
+  });
+  return mappedBinary.join("");
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
